caeabdd9eec99c48ec4615ededa44e3d4a2eca99
sdevine188 <sdevine188@gmail.com>
2016-06-16 21:02:06 -0400

    updated rough draft with datatable working basic functionality

--- eda_grants_viewer.js
+++ eda_grants_viewer.js
@@ -1,11 +1,138 @@
 var counties = counties_json2()
 var criteria = ["Unemployment", "Per capita income"]
 var selected_criteria = "Unemployment"
-var data
+var csv_data
+var data_array = []
+var table
+
+var data2 = [
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+    [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
+    [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
+    [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
+    [ "Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000" ],
+    [ "Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500" ],
+    [ "Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900" ],
+    [ "Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500" ],
+    [ "Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600" ],
+    [ "Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560" ],
+    [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
+    [ "Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600" ],
+    [ "Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500" ],
+    [ "Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750" ],
+    [ "Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500" ],
+    [ "Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000" ],
+    [ "Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500" ],
+    [ "Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000" ],
+    [ "Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500" ],
+    [ "Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000" ],
+    [ "Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000" ],
+    [ "Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450" ],
+    [ "Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600" ],
+    [ "Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000" ],
+    [ "Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575" ],
+    [ "Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650" ],
+    [ "Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850" ],
+    [ "Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000" ],
+    [ "Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000" ],
+    [ "Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400" ],
+    [ "Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500" ],
+    [ "Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000" ],
+    [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
+    [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
+    [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+];
 
 $(document).ready(function(){
 
- 	// parse uploaded csv into data variable
+	// var test_array = [["val1", "val2", "val3"], ["val4", "val5", "val6"]]
+	// // console.log(test_array)
+	// test_array = [test_array, ["val7", "val8", "val9"]]
+	// console.log(test_array)
+
+	// var fakeArray = { "length": 2, 0: "Addy", 1: "Subtracty" };
+ // 	console.log(fakeArray)
+
+	// // Therefore, convert it to a real array
+	// var realArray = $.makeArray( fakeArray )
+	// console.log(realArray)
+
+	// var o = [{
+	// 	    "proj_address": "Beattyville, KY, 00000",
+	// 	    "Proj.ST.Abbr": "KY",
+	// 	    "Proj.ZIP": "00000"
+	// 	  },
+	// 	  {
+	// 	    "proj_address": "Fargo, ND, 00000",
+	// 	    "Proj.ST.Abbr": "ND",
+	// 	    "Proj.ZIP": "00000"
+	// 	  },
+	// 	  {
+	// 	    "proj_address": "Shawneetown, IL, 62984",
+	// 	    "Proj.ST.Abbr": "IL",
+	// 	    "Proj.ZIP": "62984"
+	// 	  }]
+
+	// var data_array = []
+	// for(i = 0; i < o.length; i++){
+	// 	// console.log(_.values(o[i]))
+	// 	data_array.push(_.values(o[i]))
+	// 	// eval("var temp" + i + "=" + temp_val + ";");
+	// 	console.log(data_array)
+	// }
+
+	// var pageNumber = 1;
+	// eval("var text" + pageNumber + "=123;")
+	// console.log(text1)
+	// console.log(o)
+	// var o2 = _.map(o, function(num, key) { num })
+	// console.log(o2)
+
+	// var test = _.map({one: 1, two: 2, three: 3}, function(num, key){ return num * 3})
+	// var test = _.map([{
+	// 	    "proj_address": "Beattyville, KY, 00000",
+	// 	    "Proj.ST.Abbr": "KY",
+	// 	    "Proj.ZIP": "00000"
+	// 	  }, {
+	// 	    "proj_address": "Fargo, ND, 00000",
+	// 	    "Proj.ST.Abbr": "ND",
+	// 	    "Proj.ZIP": "00000"
+	// 	  }], function(val){ return _.values})
+
+	// console.log(test)
+
+	// var test2 = _.map(test, function(val, key){return val})
+	// console.log(test2)
+	// console.log(data2)
+
+	// var o = {"0":"1","1":"2","2":"3","3":"4"};
+	// var o = {"0":"1","1":"2","2":"3","3":"4"};
+	// console.log(o)
+
+	// var o_array = $.makeArray(o)
+	// console.log(o_array)
+
+	// var arr = $.map(o, function(el) { return el; })
+	// console.log(arr)
+
+
+	// $('#csv-table').DataTable({
+ //      	data: data,
+ //      	columnDefs: [
+ //        		{ targets: [0, 5], visible: true}
+ //        	]
+ //      	// columns: [
+	//       //       { title: "Name" },
+	//       //       { title: "Position" },
+	//       //       { title: "Office" },
+	//       //       { title: "Extn." },
+	//       //       { title: "Start date" },
+	//       //       { title: "Salary" }
+ //       //  	]
+ //    	});
+
+	// parse uploaded csv into data variable
 	function handleFileSelect(evt) {
 		var file = evt.target.files[0];
 
@@ -13,10 +140,46 @@ $(document).ready(function(){
 			header: true,
 			dynamicTyping: true,
 			complete: function(results) {
-				data = results;
+				console.log("in parse function");
+				csv_data = results.data;
+				console.log(csv_data.length)
+				convert_obj_to_array()
+				// console.log(data_array)
+				create_table()
+				// data = results.toArrays()
+				// table = $('#csv-table').DataTable({
+			 //      	data: data,
+			 //      	columnDefs: [
+			 //        		{ targets: [0, 5], visible: true}
+			 //        	]
+				// })
+				// table.destroy()
+				// table.clear().draw()
+				// table.row.add(data)
+				// create_table()
 			}
 		});
 	}
+
+	function convert_obj_to_array(data) {
+		console.log("in convert function")
+		console.log(csv_data.length)
+		for(i = 0; i < csv_data.length - 1; i++){
+			data_array.push(_.values(csv_data[i]))
+		}
+	}
+
+	function create_table() {
+		console.log("in create_table function")
+		table = $('#csv-table').DataTable({
+		      	data: data_array,
+		      	columnDefs: [
+		        		{ targets: [0, 2], visible: true}
+		        	]
+		})
+	}
+
+	// create_table()
 
 	$("#csv-file").change(handleFileSelect);
 
